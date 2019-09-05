2737ea8c187993345a40320aabd82d0967f21137
xieweiyao <451066429@qq.com>
2019-09-05 17:39:03 +0800

    抽取引擎工具类

---
+++ src/main/java/cn/guolf/activiti/Utills/ProcessUtills.java
@@ -0,0 +1,34 @@
+package cn.guolf.activiti.Utills;
+
+import org.activiti.engine.*;
+import org.activiti.engine.impl.cfg.StandaloneProcessEngineConfiguration;
+
+/**
+ * @Auther: xwy
+ * @Date: 2019/9/5 17:37
+ * @Description: 引擎工具类
+ */
+public class ProcessUtills {
+    //创建流程引擎
+    public static ProcessEngine newEngine(){
+        ProcessEngineConfiguration cfg = new StandaloneProcessEngineConfiguration()
+                .setJdbcUrl("jdbc:oracle:thin:@127.0.0.1:1521:orcl").setJdbcUsername("DG").setJdbcPassword("Compass!23$.")
+                .setJdbcDriver("oracle.jdbc.driver.OracleDriver")
+                .setJobExecutorActivate(true)
+                .setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE);
+        ProcessEngine processEngine = cfg.buildProcessEngine();
+        return processEngine;
+    }
+    public static RepositoryService repositoryService(){
+        return newEngine().getRepositoryService();
+    }
+    public static IdentityService identityService(){
+        return newEngine().getIdentityService();
+    }
+    public static RuntimeService runtimeService(){
+        return newEngine().getRuntimeService();
+    }
+    public static TaskService taskService(){
+        return newEngine().getTaskService();
+    }
+}
