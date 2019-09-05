2f7ac5a342aaaefdfe7aa41f649ab0a721adc71b
xieweiyao <451066429@qq.com>
2019-09-05 17:39:46 +0800

    修改并行1示例

--- src/main/java/cn/guolf/activiti/ParallelProcess/Parallel1.java
+++ src/main/java/cn/guolf/activiti/ParallelProcess/Parallel1.java
@@ -1,50 +1,31 @@
 package cn.guolf.activiti.ParallelProcess;
 
-import org.activiti.engine.*;
-import org.activiti.engine.impl.cfg.StandaloneProcessEngineConfiguration;
+import cn.guolf.activiti.Utills.ProcessUtills;
+import org.activiti.engine.RepositoryService;
 import org.activiti.engine.repository.Deployment;
 import org.activiti.engine.repository.ProcessDefinition;
 import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.engine.task.Task;
 import org.junit.Test;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @Auther: xwy
  * @Date: 2019/9/5 16:02
- * @Description: 并行任务,并行节点设置了完成条件
+ * @Description: 并行任务, 并行节点设置了完成条件
  */
 public class Parallel1 {
-    //创建流程引擎
-    public ProcessEngine newEngine(){
-        ProcessEngineConfiguration cfg = new StandaloneProcessEngineConfiguration()
-                .setJdbcUrl("jdbc:oracle:thin:@127.0.0.1:1521:orcl").setJdbcUsername("DG").setJdbcPassword("Compass!23$.")
-                .setJdbcDriver("oracle.jdbc.driver.OracleDriver")
-                .setJobExecutorActivate(true)
-                .setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE);
-        ProcessEngine processEngine = cfg.buildProcessEngine();
-        return processEngine;
-    }
-    public RepositoryService repositoryService(){
-        return newEngine().getRepositoryService();
-    }
-    public IdentityService identityService(){
-        return newEngine().getIdentityService();
-    }
-    public RuntimeService runtimeService(){
-        return newEngine().getRuntimeService();
-    }
-    public TaskService taskService(){
-        return newEngine().getTaskService();
-    }
 
     /**
      * 发布
      */
     @Test
-    public void deploy(){
-        RepositoryService repositoryService = repositoryService();
+    public void deploy() {
+        RepositoryService repositoryService = ProcessUtills.repositoryService();
         // 流程部署
         Deployment deployment = repositoryService.createDeployment().addClasspathResource("Parallel1.bpmn")
                 .deploy();
@@ -55,12 +36,12 @@ public class Parallel1 {
     }
 
     @Test
-    public void start(){
+    public void start() {
 // 设置流程创建人
-        identityService().setAuthenticatedUserId("xwy");
+        ProcessUtills.identityService().setAuthenticatedUserId("xwy");
 
         // 收文登记，启动流程
-        ProcessInstance processInstance = runtimeService()
+        ProcessInstance processInstance = ProcessUtills.runtimeService()
                 .startProcessInstanceByKey("myProcess_1", "Key001");
 
         System.out.println("流程实例ID = " + processInstance.getId());
@@ -69,20 +50,20 @@ public class Parallel1 {
     }
 
     @Test
-    public void complete(){
-        List<Task> taskList = taskService().createTaskQuery()
+    public void complete() {
+        List<Task> taskList = ProcessUtills.taskService().createTaskQuery()
                 .taskAssignee("xwy").orderByTaskCreateTime().desc().list();
         System.out.println("taskList = " + taskList);
         //指定多实例节点的处理对象
-        List<String> userList = Arrays.asList("xwy","yh","pp");
+        List<String> userList = Arrays.asList("xwy", "yh", "pp");
         Map vars = new HashMap();
-        vars.put("userList",userList);
-        taskService().complete(taskList.get(0).getId(),vars);
+        vars.put("userList", userList);
+        ProcessUtills.taskService().complete(taskList.get(0).getId(), vars);
     }
 
     @Test
-    public void completeOne(){
-        List<Task> taskList = taskService().createTaskQuery()
+    public void completeOne() {
+        List<Task> taskList = ProcessUtills.taskService().createTaskQuery()
                 .taskAssignee("xwy").orderByTaskCreateTime().desc().list();
 //        List<Task> taskList = taskService().createTaskQuery()
 //                .taskAssignee("yh").orderByTaskCreateTime().desc().list();
@@ -90,7 +71,7 @@ public class Parallel1 {
 //                .taskAssignee("pp").orderByTaskCreateTime().desc().list();
         System.out.println("taskList = " + taskList);
         //指定多实例节点的处理对象
-        taskService().complete(taskList.get(0).getId());
+        ProcessUtills.taskService().complete(taskList.get(0).getId());
     }
 
 }
